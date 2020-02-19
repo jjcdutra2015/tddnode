bcf139780572bbe15fb51b79c58e8194aac8adce
Julio Dutra <jjcdutra@gmail.com>
2020-02-19 20:33:01 -0300

    Criptografia de senha

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,9 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
 import truncate from '../util/truncate';
+import User from '../../src/app/models/User';
 
 describe('User', () => {
   beforeEach(async () => {
@@ -20,13 +22,25 @@ describe('User', () => {
     expect(response.body).toHaveProperty('id');
   });
 
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Julio Dutra',
+      email: 'julio@rocketseat.com.br',
+      password: '1234567',
+    });
+
+    const compareHash = await bcrypt.compare('1234567', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register with duplicated email', async () => {
     await request(app)
       .post('/users')
       .send({
         name: 'Julio Dutra',
         email: 'julio@rocketseat.com.br',
-        password_hash: '1234567',
+        password: '1234567',
       });
 
     const response = await request(app)
@@ -34,7 +48,7 @@ describe('User', () => {
       .send({
         name: 'Julio Dutra',
         email: 'julio@rocketseat.com.br',
-        password_hash: '1234567',
+        password: '1234567',
       });
 
     expect(response.status).toBe(400);
