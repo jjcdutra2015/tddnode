5eb46923088e9fbc76df4c917401ae5ecb1ae2e0
Julio Dutra <jjcdutra@gmail.com>
2020-02-19 20:55:05 -0300

    Gerando dados aleatórios

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -3,29 +3,15 @@ import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
 import truncate from '../util/truncate';
-import User from '../../src/app/models/User';
+import factory from '../factories';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
 
-  it('should be able to register', async () => {
-    const response = await request(app)
-      .post('/users')
-      .send({
-        name: 'Julio Dutra',
-        email: 'julio@rocketseat.com.br',
-        password_hash: '1234567',
-      });
-
-    expect(response.body).toHaveProperty('id');
-  });
-
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'Julio Dutra',
-      email: 'julio@rocketseat.com.br',
+    const user = await factory.create('User', {
       password: '1234567',
     });
 
@@ -34,22 +20,26 @@ describe('User', () => {
     expect(compareHash).toBe(true);
   });
 
-  it('should be able to register with duplicated email', async () => {
-    await request(app)
-      .post('/users')
-      .send({
-        name: 'Julio Dutra',
-        email: 'julio@rocketseat.com.br',
-        password: '1234567',
-      });
+  it('should be able to register', async () => {
+    const user = await factory.attrs('User');
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Julio Dutra',
-        email: 'julio@rocketseat.com.br',
-        password: '1234567',
-      });
+      .send(user);
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
 
     expect(response.status).toBe(400);
   });
